1514392b804d180d1acaaa0a62f95dae2540335d
mxe <110362553+ImMaxence@users.noreply.github.com>
2022-11-14 18:44:16 +0100

    Add files via upload

---
+++ front/src/components/Error_page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error_page = () => {
+    return (
+        <div>
+            <h1>BAD URL SORRY BRO</h1>
+        </div>
+    );
+};
+
+export default Error_page;
