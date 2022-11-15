ed8bb92c32a86643d3f0d5d77263fab926808024
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2022-11-15 12:32:51 +0100

    Error Page

--- front/src/components/Error_page.jsx
+++ front/src/components/Error_page.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error_page = () => {
     return (
-        <div>
-            <h1>BAD URL SORRY BRO</h1>
-        </div>
+        <>
+            <div className="container_error">
+                <div className="title_login">
+                    <img src="./img/logonoir.png" alt="logo not load" />
+                    <div className="just_for_title">
+                        <h1>LOOK FOR YOUR ATHLETE</h1>
+                    </div>
+                    <p id='responsive_margin'>2024 SUMMER OLYMPICS</p>
+                </div>
+                <div className='error_message'>
+                    <h1>Page Introuvable</h1>
+                    <p>
+                        <Link to="/" style={{textDecoration: 'none'}}>Retourner à l'accueil</Link>
+                    </p>
+                </div>
+            </div>
+        </>
     );
 };
 
