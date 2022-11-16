d47d1f386a3cd357d43ab88e8083bcfb155c33fa
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2022-11-16 11:45:04 +0100

    error page

--- front/src/components/Error_page.jsx
+++ front/src/components/Error_page.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 const Error_page = () => {
     return (
-        <>
+        <div className='pre_container_error'>
+            <Header/>
             <div className="container_error">
-
+                <div className="title_login">
+                    <img src="./img/logonoir.png" alt="logo not load" />
+                    <div className="just_for_title">
+                        <h1>LOOK FOR YOUR ATHLETE</h1>
+                    </div>
+                    <p id='responsive_margin'>2024 SUMMER OLYMPICS</p>
+                </div>
                 <div className='error_message'>
                     <h1>Page Introuvable</h1>
                     <p>
-                        <Link to="/" style={{ textDecoration: 'none' }}>Retourner au Login</Link>
+                        <Link to="/home" style={{textDecoration: 'none'}}>Retour à l'accueil</Link>
                     </p>
                     <p>
-                        <Link to="/home" style={{ textDecoration: 'none' }}>Retourner à l'accueil</Link>
+                        <Link to="/" style={{textDecoration: 'none'}}>Retour au login</Link>
                     </p>
                 </div>
             </div>
-        </>
+            <Footer/>
+        </div>
     );
 };
 
